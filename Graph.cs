a9834fd752551af9388334c2c45f895cbc74f892
Özgür Sarıkaya <ozgursarikaya@gmail.com>
2024-02-27 22:41:18 +0300

    Add project files.

---
+++ Graph.cs
@@ -0,0 +1,63 @@
+﻿namespace BellmanFord
+{
+    public class Graph
+    {
+        public int V, E; // Number of vertices and edges
+        public Edge[] edges; // Array to store all edges
+
+        public Graph(int v, int e)
+        {
+            V = v;
+            E = e;
+            edges = new Edge[E];
+            for (int i = 0; i < e; ++i)
+                edges[i] = new Edge();
+        }
+
+        // Utility function to print the solution
+        private void Print(int[] distance)
+        {
+            Console.WriteLine("Vertex Distance from Source:");
+            for (int i = 0; i < V; ++i)
+                Console.WriteLine("{0}\t\t{1}", i, distance[i]);
+        }
+        // The main function to find the shortest distances from source to all other vertices using Bellman-Ford algorithm
+        public void BellmanFordAlgorithm(int source)
+        {
+            int[] distance = new int[V];
+
+            // Step 1: Initialize distances from source to all other vertices as INFINITE
+            for (int i = 0; i < V; ++i)
+                distance[i] = int.MaxValue;
+            distance[source] = 0;
+
+            // Step 2: Relax all edges |V| - 1 times. A simple shortest path from source to any other vertex can have at most |V| - 1 edges
+            for (int i = 1; i < V; ++i)
+            {
+                foreach (Edge edge in edges)
+                {
+                    int u = edge.Source;
+                    int v = edge.Destination;
+                    int weight = edge.Weight;
+                    if (distance[u] != int.MaxValue && distance[u] + weight < distance[v])
+                        distance[v] = distance[u] + weight;
+                }
+            }
+
+            // Step 3: Check for negative-weight cycles. The above step guarantees the shortest distances if the graph doesn't contain a negative-weight cycle. If we get a shorter path, then there is a cycle
+            foreach (Edge edge in edges)
+            {
+                int u = edge.Source;
+                int v = edge.Destination;
+                int weight = edge.Weight;
+                if (distance[u] != int.MaxValue && distance[u] + weight < distance[v])
+                {
+                    Console.WriteLine("Graph contains negative weight cycle");
+                    return;
+                }
+            }
+
+            Print(distance);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using BellmanFord;
+
+int V = 5; // Number of vertices in the graph
+int E = 8; // Number of edges in the graph
+
+Graph graph = new Graph(V, E);
+
+// Example graph
+graph.edges[0].Source = 0;
+graph.edges[0].Destination = 1;
+graph.edges[0].Weight = -1;
+
+graph.edges[1].Source = 0;
+graph.edges[1].Destination = 2;
+graph.edges[1].Weight = 4;
+
+graph.edges[2].Source = 1;
+graph.edges[2].Destination = 2;
+graph.edges[2].Weight = 3;
+
+graph.edges[3].Source = 1;
+graph.edges[3].Destination = 3;
+graph.edges[3].Weight = 2;
+
+graph.edges[4].Source = 1;
+graph.edges[4].Destination = 4;
+graph.edges[4].Weight = 2;
+
+graph.edges[5].Source = 3;
+graph.edges[5].Destination = 2;
+graph.edges[5].Weight = 5;
+
+graph.edges[6].Source = 3;
+graph.edges[6].Destination = 1;
+graph.edges[6].Weight = 1;
+
+graph.edges[7].Source = 4;
+graph.edges[7].Destination = 3;
+graph.edges[7].Weight = -3;
+
+int source = 0; // Source vertex
+
+Console.WriteLine("Bellman-Ford algorithm results:");
+graph.BellmanFordAlgorithm(source);
